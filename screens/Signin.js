531b0993f78edb5b8ae1d75c272eecb808751d48
promise anamonye <promiseanamonye@gmail.com>
2022-12-12 12:28:52 +0100

    changes to meta data

---
+++ screens/Signin.js
@@ -0,0 +1,11 @@
+import { View,Text,ScrollView,StyleSheet } from "react-native";
+import { SafeArea } from '../Utils/safearea';
+
+
+export function Signin () {
+    return (
+    <SafeArea>
+        <Text>Sign in</Text>
+        </SafeArea>
+    )
+}
